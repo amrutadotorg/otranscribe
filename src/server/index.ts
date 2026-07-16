a5f7ebd3abcb6ea49fbee383984fe67c28b3b51a
amrutadotorg <amruta.org@gmail.com>
2026-07-16 20:37:53 +0200

    fix: update Express catchall route for path-to-regexp v8

--- src/server/index.ts
+++ src/server/index.ts
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, '../dist')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (_req, res) => {
+app.get('/{*splat}', (_req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
